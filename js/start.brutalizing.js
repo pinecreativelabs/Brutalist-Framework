c09d43d9191ac319ddaed71c9a9f2f9d9b9766c1
Pine Creative Labs <brad.fogelstrom@gmail.com>
2017-01-09 18:23:06 -0500

    Flavors Released

---
+++ js/start.brutalizing.js
@@ -0,0 +1,16 @@
+﻿/* Let the Brutalization Begin */
+/* NOTE: This script is must be UTF-8 encoded, so as to ensure character rendering on Baffle */
+
+/* FitText */
+$(".fittext").fitText();
+$(".fittext-compress").fitText(2.2); /* Increases compression */
+$(".fittext-uncompress").fitText(0.7); /* Reduces compression */
+	
+/* Baffle */
+let b = baffle('.baffle', {
+	characters: '█▓▒░', speed: 150
+}).reveal(2500);
+let bf = baffle('.baffle-forever', {characters: '█▓▒░'}).start();
+someAsyncFunction(result => {
+    bf.text(currentText => result.text).reveal(2500);
+});
