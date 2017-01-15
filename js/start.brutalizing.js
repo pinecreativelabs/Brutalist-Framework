4d8185e16e8af2cbcafd39f94ba5ebba121c62dd
Pine Creative Labs <brad.fogelstrom@gmail.com>
2017-01-14 22:52:10 -0500

    BETA 1
    First BETA release.

--- js/start.brutalizing.js
+++ js/start.brutalizing.js
@@ -1,16 +1,14 @@
 ﻿/* Let the Brutalization Begin */
-/* NOTE: This script is must be UTF-8 encoded, so as to ensure character rendering on Baffle */
+/* NOTE: This script is must be UTF-8 encoded, so as to ensure obfuscated character rendering on Baffle */
 
-/* FitText */
+/* FITTEXT */
 $(".fittext").fitText();
 $(".fittext-compress").fitText(2.2); /* Increases compression */
-$(".fittext-uncompress").fitText(0.7); /* Reduces compression */
+$(".fittext-uncompress").fitText(0.8); /* Reduces compression */
 	
-/* Baffle */
-let b = baffle('.baffle', {
-	characters: '█▓▒░', speed: 150
-}).reveal(2500);
-let bf = baffle('.baffle-forever', {characters: '█▓▒░'}).start();
-someAsyncFunction(result => {
-    bf.text(currentText => result.text).reveal(2500);
-});
+/* BAFFLE */
+/* Animation speed value can be increased or decreased. Same with duration */
+let b = baffle('.baffle', { characters: '█▓▒░', speed: 150 }).reveal(2500); /* Runs animation only once, upon page load */
+let bl = baffle('.baffle-longer', { characters: '█▓▒░', speed: 150, duration: 3000 }).reveal(2500); /* Animation duration set to 3 seconds */
+let bf = baffle('.baffle-forever', {characters: '█▓▒░', speed: 150}).start(); /* Animation continues on an endless loop */
+
