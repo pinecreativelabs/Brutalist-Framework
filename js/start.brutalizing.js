ae5eaeb66237001b681f0667506937dc1c109301
Pine Creative Labs <brad.fogelstrom@gmail.com>
2017-05-22 22:31:47 -0400

    1.1
    Added Flexbox grid, simple menu, 2 new flavors, improvements to fittext, baffle, typography, and borders and separators.

--- js/start.brutalizing.js
+++ js/start.brutalizing.js
@@ -7,6 +7,12 @@ $(document).ready(function(){
 $(".fittext").fitText();
 $(".fittext-compress").fitText(2.2); /* Increases compression */
 $(".fittext-uncompress").fitText(0.8); /* Reduces compression */
+$(".fittext-h1").fitText(1.5, { minFontSize: '50px', maxFontSize: '88px' }); /* Fittext ideal for H1 tag */
+$(".fittext-h2").fitText(1.5, { minFontSize: '42px', maxFontSize: '76px' }); /* Fittext ideal for H2 tag */
+$(".fittext-h3").fitText(1.5, { minFontSize: '38px', maxFontSize: '64px' }); /* Fittext ideal for H3 tag */
+$(".fittext-h4").fitText(1.5, { minFontSize: '24px', maxFontSize: '42px' }); /* Fittext ideal for H4 tag */
+$(".fittext-h5").fitText(1.5, { minFontSize: '16px', maxFontSize: '30px' }); /* Fittext ideal for H5 tag */
+$(".fittext-h6").fitText(1.5, { minFontSize: '12px', maxFontSize: '24px' }); /* Fittext ideal for H6 tag */
 
 /* STACKS */
 $('.stack-auto').each(function(index) {
@@ -21,13 +27,14 @@ $('.equal-height').matchHeight({
     remove: false
 });
 	
-/* Prideify - rainbow color filter*/
-$('.pride').prideify();
-	
+/* Prideify - rainbow color filter */
+$('.pride').prideify({ crossOriginProxy: true });
+
 /* BAFFLE */
 /* Animation speed value can be increased or decreased. Same with duration */
 let b = baffle('.baffle', { characters: '█▓▒░', speed: 150 }).reveal(2500); /* Runs animation only once, upon page load */
-let bl = baffle('.baffle-longer', { characters: '█▓▒░', speed: 150, duration: 3000 }).reveal(2500); /* Animation duration set to 3 seconds */
+let bl = baffle('.baffle-longer', { characters: '█▓▒░', speed: 150, duration: 3000 }).reveal(3000); /* Animation duration set to 3 seconds */
+let bp = baffle('.baffle-prolonged', { characters:'█▓▒░', speed: 300, duration: 10000 }).reveal(10000); /* Animation duration set to 10 seconds */
 let bf = baffle('.baffle-forever', {characters: '█▓▒░', speed: 150}).start(); /* Animation continues on an endless loop */
 
 });
