6845c37fa46a82cfb2f79d30a52a9a9c5c7f105f
Pine Creative Labs <brad.fogelstrom@gmail.com>
2017-07-22 15:21:18 -0400

    1.3

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Brutalist-Framework
 A framework for the brutalist web design trend.
 ![BF Cover](http://www.brutalistframework.com/assets/images/bf-cover.jpg)
+## About
+_Brutalist Framework_ is a responsive framework exclusively for the popular brutalist web design trend. It is intended to be a boilerplate for creating simple brutalist websites.
+
+When dealing with brutalism, we like to think of it as being CRUDE:
+* Cynically confrontational
+* Roughly raw and rugged
+* Uncomfortably ugly and unpolished
+* Deliberately daring and damaging
+* Extremely exasperated and embittered
--- js/start.brutalizing.js
+++ js/start.brutalizing.js
@@ -32,9 +32,9 @@ $('.pride').prideify({ crossOriginProxy: true });
 
 /* BAFFLE */
 /* Animation speed value can be increased or decreased. Same with duration */
-let b = baffle('.baffle', { characters: '█▓▒░', speed: 150 }).reveal(2500); /* Runs animation only once, upon page load */
-let bl = baffle('.baffle-longer', { characters: '█▓▒░', speed: 150, duration: 3000 }).reveal(3000); /* Animation duration set to 3 seconds */
-let bp = baffle('.baffle-prolonged', { characters:'█▓▒░', speed: 300, duration: 10000 }).reveal(10000); /* Animation duration set to 10 seconds */
-let bf = baffle('.baffle-forever', {characters: '█▓▒░', speed: 150}).start(); /* Animation continues on an endless loop */
+var b = baffle('.baffle', { characters: '█▓▒░', speed: 150 }).reveal(2500); /* Runs animation only once, upon page load */
+var bl = baffle('.baffle-longer', { characters: '█▓▒░', speed: 150, duration: 3000 }).reveal(3000); /* Animation duration set to 3 seconds */
+var bp = baffle('.baffle-prolonged', { characters:'█▓▒░', speed: 300, duration: 10000 }).reveal(10000); /* Animation duration set to 10 seconds */
+var bf = baffle('.baffle-forever', {characters: '█▓▒░', speed: 150}).start(); /* Animation continues on an endless loop */
 
 });
