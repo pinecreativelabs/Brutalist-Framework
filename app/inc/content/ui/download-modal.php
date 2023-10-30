2ba5d2b22384787ea43511995d43e173e2e272b3
Pine Creative Labs <brad.fogelstrom@gmail.com>
2023-10-30 15:34:28 -0400

    3.0

--- app/inc/content/ui/download-modal.php
+++ app/inc/content/ui/download-modal.php
@@ -2,16 +2,16 @@
 	<div class="infinitile padded bitstream" style="width: 80vw; max-width: 468px;">
 		<h4 class="flow-text heavy center">Begin the <span class="blink red-t">Brutality</span></h5>
 		<p class="bigger bold" style="margin-bottom: 0.75em; margin-top: 1em;">
-			<a href="http://www.brutalistframework.com/versions/Brutalist_Framework_3.0.zip" target="_blank" class="box-s btn full-btn sweden invert-h rounded">Download 3.0</a>
+			<a href="https://www.brutalistframework.com/versions/Brutalist_Framework_3.0.zip" target="_blank" class="box-s btn full-btn sweden invert-h rounded">Download 3.0</a>
 		</p>
 		<p style="margin-bottom: 1em; text-align: center;">Released <strong>10/31/2023</strong></p>
 		<hr class="charlie" />
 		<p style="font-size: 1.15em; margin-top: 0.75em;"><strong><span style="font-size: 1.3em;">Archived Versions</span></strong><br />
-			<a href="http://www.brutalistframework.com/versions/Brutalist_Framework_2.4.zip" target="_blank">2.4</a> <small>(3/24/2022), 18mb</small><br />
-			<a href="http://www.brutalistframework.com/versions/Brutalist_Framework_2.3.3.zip" target="_blank">2.3.3</a> <small>(3/11/2022), 17mb</small><br />
-			<a href="http://www.brutalistframework.com/versions/Brutalist_Framework_2.2.zip" target="_blank">2.2</a> <small>(2/14/2021), 17mb</small><br />
-			<a href="http://www.brutalistframework.com/versions/Brutalist_Framework_2.1.zip" target="_blank">2.1</a> <small>(10/23/2020), 19mb</small><br />
-			<a href="http://www.brutalistframework.com/versions/Brutalist_Framework_2.0.zip" target="_blank">2.0.2.0</a> <small>(2/20/2020), 9.3mb</small><br />
+			<a href="https://www.brutalistframework.com/versions/Brutalist_Framework_2.4.zip" target="_blank">2.4</a> <small>(3/24/2022), 18mb</small><br />
+			<a href="https://www.brutalistframework.com/versions/Brutalist_Framework_2.3.3.zip" target="_blank">2.3.3</a> <small>(3/11/2022), 17mb</small><br />
+			<a href="https://www.brutalistframework.com/versions/Brutalist_Framework_2.2.zip" target="_blank">2.2</a> <small>(2/14/2021), 17mb</small><br />
+			<a href="https://www.brutalistframework.com/versions/Brutalist_Framework_2.1.zip" target="_blank">2.1</a> <small>(10/23/2020), 19mb</small><br />
+			<a href="https://www.brutalistframework.com/versions/Brutalist_Framework_2.0.zip" target="_blank">2.0.2.0</a> <small>(2/20/2020), 9.3mb</small><br />
 		</p>
 	</div>
 </div>
