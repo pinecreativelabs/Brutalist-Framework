5b756168919f752d8cbd2c3ad25beb59facacd1c
Pine Creative Labs <brad.fogelstrom@gmail.com>
2024-04-04 14:13:40 -0400

    3.0.3
    bekah.php bug fix

--- core/bekah.php
+++ core/bekah.php
@@ -232,18 +232,19 @@ $user_curc = $ipdat->geoplugin_currencyCode;
 $user_curs = $ipdat->geoplugin_currencySymbol;
 
 // get browser language
-function get_client_language($availableLanguages, $default_lang='en'){
-	if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-		$langs=explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
-		foreach ($langs as $value){
-			$choice=substr($value,0,2);
-			if(in_array($choice, $availableLanguages)){
-				return $choice;
-			}
+function get_browser_language( $available = [], $default = 'en' ) {
+	if ( isset( $_SERVER[ 'HTTP_ACCEPT_LANGUAGE' ] ) ) {
+		$langs = explode( ',', $_SERVER['HTTP_ACCEPT_LANGUAGE'] );
+		if ( empty( $available ) ) {
+		  return empty( $langs ) ? $default : $langs[ 0 ];
 		}
-	} 
-	return $default_lang;
+		foreach ( $langs as $lang ){
+			$lang = substr( $lang, 0, 2 );
+			if( in_array( $lang, $available ) ) {return $lang;}
+		}
+	}
+	return $default;
 }
-$browser_lang = get_client_language();
+$browser_lang = get_browser_language();
 
 ?>
