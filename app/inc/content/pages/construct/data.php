5ec0ad985162368cd3e448eff64a0210de1f0c37
Pine Creative Labs <brad.fogelstrom@gmail.com>
2024-05-02 11:24:00 -0400

    3.0.3

---
+++ app/inc/content/pages/construct/data.php
@@ -0,0 +1,22 @@
+<h3 class="fluid-text monolisk" id="data" style="margin-bottom: 0;"><strong>Data</strong></h3>
+<p class="bitstream">The BF CORE contains data files that can be used for various purposes.</p>
+<div class="flex bitstream"><div class="padded half">
+	<h4 class="charcoal vanilla-t">Data File Types</h4>
+	<p style="margin-bottom: 1em;">There are four types of data stored in their respective directories:</p>
+	<p class="padded disabled box-s files" style="font-size: 1.33rem;">
+		<i class="csv"></i> <strong>bos/core/data/csv/</strong> <small>(CSV files)</small><br />
+		<i class="json"></i> <strong>bos/core/data/json/</strong> <small>(JSON files)</small><br />
+		<i class="sql"></i> <strong>bos/core/data/sql/</strong> <small>(SQL files)</small><br />
+		<i class="xml"></i> <strong>bos/core/data/xml/</strong> <small>(XML files)</small>
+	</p>
+</div><div class="padded half">
+	<h4 class="charcoal vanilla-t">Using Data Files</h4>
+	<p>The included data files are used in conjunction with PHP constructors to create prepopulated dropdown selectors, such as a country selector. The following data is included:</p>
+	<ul class="check">
+		<li>Country names / codes / currencies</li>
+		<li>States &amp; provinces (Canada)</li>
+		<li>Timezones</li>
+		<li>Languages</li>
+	</ul>
+</div></div>
+<p class="spacer"></p>
