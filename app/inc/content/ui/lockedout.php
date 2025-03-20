c6015e0602245a7a7780778079bb53d0ab54c380
Pine Creative Labs <brad.fogelstrom@gmail.com>
2025-03-20 13:35:20 -0400

    2.5.3

--- app/inc/content/ui/lockedout.php
+++
@@ -1,6 +0,0 @@
-<div id="lockedout" style="display:none;">
-	<div class="padded alert bitstream b-b-k center" style="width: 50vw; max-width: 768px;">
-		<h4 class="flow-text heavy"><i class="bi bi-lock"></i> LOCKED OUT</h5>
-		<p>This feature will be available in <strong>3.1</strong> (or later).</p>
-	</div>
-</div>
